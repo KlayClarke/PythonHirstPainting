e4e2340387e233870e8ec0db27716533835e62ef
Klay Clarke <klayaclarke@gmail.com>
2021-11-15 20:38:06 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# import colorgram
+#
+# colors = colorgram.extract('spot_painting.jpeg', 10)
+#
+# list_of_colors = []
+#
+# for color in colors:
+#     list_of_colors.append((color.rgb.r, color.rgb.g, color.rgb.b))
+#
+# print(list_of_colors)
+
+color_list = [(199, 175, 117), (212, 222, 215), (125, 36, 24), (167, 106, 56), (186, 159, 52), (6, 57, 83), (108, 68, 85)]
+
