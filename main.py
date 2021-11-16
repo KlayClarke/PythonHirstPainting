91695ec7a47a93223feefc9ce52f4171280487bc
Klay Clarke <klayaclarke@gmail.com>
2021-11-15 21:52:16 -0500

    create Damien Hirst replica using turtle graphics and colorgram

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-# import colorgram
-#
-# colors = colorgram.extract('spot_painting.jpeg', 10)
-#
-# list_of_colors = []
-#
-# for color in colors:
-#     list_of_colors.append((color.rgb.r, color.rgb.g, color.rgb.b))
-#
-# print(list_of_colors)
+from turtle import Turtle, Screen
+import turtle
+import random
 
-color_list = [(199, 175, 117), (212, 222, 215), (125, 36, 24), (167, 106, 56), (186, 159, 52), (6, 57, 83), (108, 68, 85)]
+tim = Turtle()
+turtle.colormode(255)
+tim.hideturtle()
+tim.penup()
+tim.speed('fastest')
 
+color_list = [(199, 175, 117), (212, 222, 215), (125, 36, 24), (167, 106, 56),
+              (186, 159, 52), (6, 57, 83), (108, 68, 85)]
+
+
+def create_line(number_of_spots_per):
+    for _ in range(number_of_spots_per):
+        r = random.choice(color_list)
+        tim.dot(20, r)
+        tim.forward(50)
+
+
+def next_line(number_of_spots_per):
+    for _ in range(number_of_spots_per):
+        tim.backward(50)
+    tim.left(90)
+    tim.forward(50)
+    tim.right(90)
+
+
+def create_art(number_of_rows, number_of_spots_per):
+    for _ in range(number_of_rows):
+        create_line(number_of_spots_per)
+        next_line(number_of_spots_per)
+
+
+create_art(5, 5)
+
+my_screen = Screen()
+my_screen.exitonclick()
